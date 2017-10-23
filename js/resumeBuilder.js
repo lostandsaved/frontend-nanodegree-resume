b6632c4fbb6b61e91dfa36934b91ef059f808f98
lostandsaved <32721941+lostandsaved@users.noreply.github.com>
2017-10-23 18:58:34 -0400

    Adding formattedRole

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,5 +3,8 @@
 //var funThoughts = awesomeThoughts.replace("awesome", "fun");
 //$("#main").append(funThoughts);
 var name="Charles Ford";
+var role="Web Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("header").append(formattedName);
+$("header").prepend(formattedRole);
