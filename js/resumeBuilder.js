936bcc55b24f0503a88d5a43e7781903d043b170
lostandsaved <32721941+lostandsaved@users.noreply.github.com>
2017-10-23 18:54:04 -0400

    First exercise

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+//$("#main").append("Charles Ford");
+//var awesomeThoughts = "I am Charlie and I am awesome!"
+//var funThoughts = awesomeThoughts.replace("awesome", "fun");
+//$("#main").append(funThoughts);
+var name="Charles Ford";
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("header").append(formattedName);
