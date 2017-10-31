133d7374b77eceefb2f377f11048b888f86721c5
lostandsaved <32721941+lostandsaved@users.noreply.github.com>
2017-10-31 19:01:05 -0400

    adding work and education for quiz
    adding work and education for quiz

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,32 +1,52 @@
-//$("#main").append("Charles Ford");
-//var awesomeThoughts = "I am Charlie and I am awesome!"
-//var funThoughts = awesomeThoughts.replace("awesome", "fun");
-//$("#main").append(funThoughts);
-//var name="Charles Ford";
-//var role="Web Developer";
-//var formattedName = HTMLheaderName.replace("%data%", name);
-//var formattedRole = HTMLheaderRole.replace("%data%", role);
-
 var bio = {
-  "name" : "Charles Ford",
-  "role" : "Web Developer",
-  "contacts" : {
-    "mobile" : "732-555-5555",
-    "email" : "charlie@notreally.com"
-    "github" : "lostandsaved",
-    "location" : "New Jersey"
+  "name": "Charles Ford",
+  "role": "Web Developer",
+  "contacts": {
+    "mobile": "732-555-5555",
+    "email": "charlie@notreally.com",
+    "github": "lostandsaved",
+    "location": "New Jersey"
   },
-  "welcomeMessage" : "This is my welcome message",
-  "skills" : [
+  "welcomeMessage": "This is my welcome message",
+  "skills": [
     "SkillOne", "SkillTwo"
   ],
-  "biopic" : "images/fry.jpg"
+  "biopic": "images/fry.jpg"
 }
+
+var work = {};
+work.position = "Web Developer";
+work.employer = "AT&T";
+work.years = "19";
+
+var education = {};
+education["name"] = "Middlesex County College";
+education["years"] = "1994 - 2000";
+education["city"] = "Edison, NJ";
+
+$("main").append(work["position"]);
+$("main").append(education.name);
+
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
 var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#topContacts, #footerContacts").append(formattedMobile + formattedEmail + formattedGithub + formattedLocation);
 $("#header").append(formattedBioPic);
 $("#header").append(formattedWelcomeMsg);
+
+//if (bio.skills.length > 0) {
+$("#header").append(HTMLskillsStart);
+for (var i = 0, biolen = bio.skills.length; i < biolen; i++) {
+  var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+//$("#header").append(formattedSkills);
+  $("#skills").append(formattedSkills);
+ }
+//}
